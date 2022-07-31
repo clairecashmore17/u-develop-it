fd7823871ac0cbb05fa0660885e2aac7089e10c3
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-31 17:20:47 -0400

    successfully connects to mysql database

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//mysql package
+const mysql = require('mysql2');
+
 // addding express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to our mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username
+        user: 'root',
+        //your password
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //GET test
 app.get('/', (req,res) => {
@@ -13,7 +29,11 @@ app.get('/', (req,res) => {
     });
 });
 
-//handle user requests not supported by app(NOT FOUND)
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+//handle user requests not supported by app(NOT FOUND) catchall route
 app.use((req,res) => {
     res.status(404).end();
 });
