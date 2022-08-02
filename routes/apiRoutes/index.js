1d93da0be031e3a7215b6b8441e80a5baf32c0bf
Claire Cashmore <clairecashmore2017@gmail.com>
2022-08-01 20:13:37 -0400

    completed routing for voter table

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -6,4 +6,8 @@ router.use(require('./candidateRoutes'));
 
 //adding the parties routing
 router.use(require('./partyRoutes'));
+
+//adding the voters routing
+router.use(require('./voterRoutes'));
+
 module.exports = router;
